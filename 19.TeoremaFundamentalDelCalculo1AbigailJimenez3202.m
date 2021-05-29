a3454513c2f4015cad0e157d178bb58ea1fda4cc
Abiiijimm1 <82452636+Abiiijimm1@users.noreply.github.com>
2021-05-29 14:50:08 -0700

    Create 19.TeoremaFundamentalDelCalculo1AbigailJimenez3202.m

---
+++ 19.TeoremaFundamentalDelCalculo1AbigailJimenez3202.m
@@ -0,0 +1,12 @@
+clc, claro
+% Abigail Jiménez Rojas 
+% Grupo: 3202
+% Teorema Fundamental de Calculo 1
+syms x;
+f = x ^ 3
+F = en línea (char (f));
+i = - 1
+r = 3
+i = int (f, x)
+disp ( ' Respuesta:  ' )
+f = int (f, x, i, f)
